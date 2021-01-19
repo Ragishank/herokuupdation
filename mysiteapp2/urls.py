1c0ca2862094f6751a5b5a492d9aae3ef0d6e2e9
Ragishank <ragisharajannk321@gmail.com>
2021-01-19 10:26:07 +0530

    updation4

--- mysiteapp2/urls.py
+++ mysiteapp2/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.fnIndex)
+    path('',views.fnIndex),
+    path('ass1',views.assginment1),
+    path('ass2',views.assginment2),
+    path('ass3',views.assginment3)
 ]
--- mysiteapp2/views.py
+++ mysiteapp2/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def fnIndex(request):
     return render(request,'index.html')
+def assginment1(request):
+    return render(request, 'assignment1.html')
+
+def assginment2(request):
+    return render(request, 'assignment2.html')
+def assginment3(request):
+    return render(request, 'assignment3.html')
